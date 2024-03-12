960b074c45a809ade2ae703c81670d5a5dab11e4
marcio <marciopivajunior457@gmail.com>
2024-03-12 16:05:26 -0300

    Feat: icon and title page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,6 @@ import Posts from "Components/Posts";
 
 export default function Home(){
     return(
-        <Posts title="Datas de comemoração" is="home"></Posts>
+        <Posts title="Datas comemorativas" is="home"></Posts>
     )
 }
